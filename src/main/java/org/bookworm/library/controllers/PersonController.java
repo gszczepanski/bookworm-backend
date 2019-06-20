7f18d4176c6a42a7be479eeb8c6414d4ca0b7fdc
gszczepanski <mailbox.g.szczepanski@gmail.com>
2019-06-20 12:38:12 +0200

    Person controller

---
+++ src/main/java/org/bookworm/library/controllers/PersonController.java
@@ -0,0 +1,24 @@
+package org.bookworm.library.controllers;
+
+import org.bookworm.library.entities.Person;
+import org.bookworm.library.repositories.IPersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Grzegorz on 2019/06/20
+ */
+@RestController
+@RequestMapping("/persons")
+public class PersonController {
+
+    @Autowired
+    IPersonRepository personRepository;
+
+    @CrossOrigin(origins = "${ws.cross.origin.address}")
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Person insert(@RequestBody Person person) {
+
+        return personRepository.save(person);
+    }
+}
