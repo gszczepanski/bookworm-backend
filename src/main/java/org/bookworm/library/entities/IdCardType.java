6afccd6ec757815bb72e556452d3fb558a8da9be
gszczepanski <mailbox.g.szczepanski@gmail.com>
2019-06-28 06:52:00 +0200

    IdCardType introduction

---
+++ src/main/java/org/bookworm/library/entities/IdCardType.java
@@ -0,0 +1,14 @@
+package org.bookworm.library.entities;
+
+import io.swagger.annotations.ApiModel;
+
+/**
+ * Created by Grzegorz on 2019/06/10
+ */
+@ApiModel
+public enum IdCardType {
+
+    NATIONAL_IDENTITY_CARD,
+    DRIVER_LICENCE,
+    INSURANCE_CARD
+}
