ea1f39cd45a0d79b1946c82066c61c486ee437b6
gszczepanski <mailbox.g.szczepanski@gmail.com>
2019-07-01 19:56:45 +0200

    BookController update

--- src/main/java/org/bookworm/library/controllers/BookController.java
+++ src/main/java/org/bookworm/library/controllers/BookController.java
@@ -1,8 +1,12 @@
 package org.bookworm.library.controllers;
 
 import org.bookworm.library.entities.Book;
+import org.bookworm.library.entities.BookStatus;
 import org.bookworm.library.repositories.IBookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -31,4 +35,27 @@ public class BookController {
         return bookRepository.save(book);
     }
 
+    @CrossOrigin(origins = "${ws.cross.origin.address}")
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Page<Book> findAll(Pageable pageable) {
+
+        return bookRepository.findAll(pageable);
+    }
+
+    @CrossOrigin(origins = "${ws.cross.origin.address}")
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteById(@PathVariable(value = "id") UUID id) {
+
+        bookRepository.deleteById(id);
+        return ResponseEntity.ok("book deleted");
+    }
+
+    @CrossOrigin(origins = "${ws.cross.origin.address}")
+    @RequestMapping(value = "/{id}/status/{status}", method = RequestMethod.PATCH)
+    public ResponseEntity<?> setStatus(@PathVariable(value = "status") BookStatus status,
+                                     @PathVariable(value = "id") UUID id) {
+
+        bookRepository.setStatus(status, id);
+        return ResponseEntity.ok("book type updated");
+    }
 }
