41f695628ee38f08367df587553e17e852bc4ea1
grzegorz <mailbox.g.szczepanski@gmail.com>
2021-11-17 08:31:38 +0100

    uuid generator update

--- src/main/java/org/bookworm/library/entities/EntityWithUUID.java
+++ src/main/java/org/bookworm/library/entities/EntityWithUUID.java
@@ -2,8 +2,11 @@ package org.bookworm.library.entities;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Type;
 
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.util.UUID;
@@ -18,6 +21,17 @@ public abstract class EntityWithUUID {
 
     @Id
     @Type(type = "pg-uuid")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator",
+            parameters = {
+                    @Parameter(
+                            name = "uuid_gen_strategy_class",
+                            value = "org.hibernate.id.uuid.CustomVersionOneStrategy"
+                    )
+            }
+    )
     private UUID id;
 
     protected EntityWithUUID() {
