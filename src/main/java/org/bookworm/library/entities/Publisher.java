8abdf95018aecd5697d590af662a663f7a11297d
grzegorz <mailbox.g.szczepanski@gmail.com>
2021-11-19 17:30:45 +0100

    publisher name unique

--- src/main/java/org/bookworm/library/entities/Publisher.java
+++ src/main/java/org/bookworm/library/entities/Publisher.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -26,5 +23,6 @@ public class Publisher {
 
     @NotNull
     @Size(max=200)
+    @Column(unique=true)
     private String name;
 }
