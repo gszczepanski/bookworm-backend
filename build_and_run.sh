936955a23d8b81f1ee8871149baa6eae8dbd82e6
grzegorz <mailbox.g.szczepanski@gmail.com>
2021-11-21 09:00:41 +0100

    build & run script update

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./gradlew bootJar --warning-mode all
 cd docker || { echo "Docker folder does not exist."; exit 1; }
-cp -f ../build/libs/bookworm-backend-0.0.1-SNAPSHOT.jar ./bookworm_backend/
+ln ../build/libs/bookworm-backend-0.0.1-SNAPSHOT.jar ./bookworm_backend/bookworm-backend-0.0.1-SNAPSHOT.jar
 cd bookworm_backend
 docker-compose up --build
-rm bookworm-backend-0.0.1-SNAPSHOT.jar
+unlink bookworm-backend-0.0.1-SNAPSHOT.jar
