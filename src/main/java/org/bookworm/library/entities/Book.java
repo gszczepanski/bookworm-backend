b602ca75344594acacfd8421fa3174805e259912
grzegorz <mailbox.g.szczepanski@gmail.com>
2021-11-23 19:14:32 +0100

    book entity update

--- src/main/java/org/bookworm/library/entities/Book.java
+++ src/main/java/org/bookworm/library/entities/Book.java
@@ -1,5 +1,6 @@
 package org.bookworm.library.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,7 +10,10 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by Grzegorz on 2019/05/13
@@ -33,7 +37,6 @@ public class Book extends EntityWithUUID {
     @NotNull
     private Integer year;
 
-    @NotNull
     @Size(max=20)
     private String volume;
 
@@ -44,10 +47,11 @@ public class Book extends EntityWithUUID {
     @Size(max=100)
     private String seriesName;
 
+    @Size(max=255)
     private String comment;
 
     @NotNull
-    @Column(insertable = false, updatable = false)
+    @Column(updatable = false)
     private LocalDate acquireDate;
 
     @Enumerated(EnumType.ORDINAL)
@@ -55,27 +59,45 @@ public class Book extends EntityWithUUID {
     private BookAcquiringMethod acquiringMethod;
 
     @NotNull
-    private Integer acquiringEmployeeId;
+    private UUID acquiringEmployeeId;
 
     @Size(max=20)
     private String invoiceSymbol;
 
     private BigDecimal price;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "book_author", joinColumns = { @JoinColumn(name = "book_id") }, inverseJoinColumns = { @JoinColumn(name = "author_id") })
-    private Set<Author> authors;
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(name = "book_author",
+            joinColumns = { @JoinColumn(name = "book_id", referencedColumnName = "id") },
+            inverseJoinColumns = { @JoinColumn(name = "author_id", referencedColumnName = "id") })
+    @JsonIgnore
+    private List<Author> authors;
 
     @Enumerated(EnumType.ORDINAL)
     @NotNull
     private BookStatus status;
 
-    @ManyToOne(optional = false, fetch = FetchType.EAGER)
-    @JoinColumn(name="publisher_id", insertable = false, updatable = false)
-    @NotNull
+    @ManyToOne(cascade = CascadeType.MERGE, optional = false, fetch = FetchType.EAGER)
+    @JoinColumn(name="publisher_id")
     private Publisher publisher;
 
     @Enumerated(EnumType.ORDINAL)
     @NotNull
     private Language language;
+
+    public Book() {
+        List authors = new ArrayList<>();
+    }
+
+    /**
+     * Optionally force client classes through your add/remove methods if mutual
+     * relationship should be maintained.
+     */
+    public List<Author> getAuthors() {
+        return Collections.unmodifiableList(authors);
+    }
+
+    public void addAuthor(Author author) {
+        authors.add(author);
+    }
 }
