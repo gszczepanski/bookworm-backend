6f618841cad3ac876ffa813d04a86cbc24e3cbba
grzegorz <mailbox.g.szczepanski@gmail.com>
2021-12-26 16:44:48 +0100

    java class with karate tests renamed

---
+++ src/test/java/org/bookworm/library/karate/LibraryKarateTests.java
@@ -0,0 +1,29 @@
+package org.bookworm.library.karate;
+
+import com.intuit.karate.junit5.Karate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+@SpringBootTest
+@SqlGroup({@Sql("/make_tables_empty.sql")})
+public class LibraryKarateTests {
+
+    @Karate.Test
+    Karate testAuthor() {
+        return Karate.run("author.feature").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testBook() { return Karate.run("book.feature").relativeTo(getClass()); }
+
+    @Karate.Test
+    Karate testPerson() {
+        return Karate.run("person.feature").relativeTo(getClass());
+    }
+
+    @Karate.Test
+    Karate testPublisher() {
+        return Karate.run("publisher.feature").relativeTo(getClass());
+    }
+}
