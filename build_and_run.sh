f6292c2d3c9f2a3fe19913eb6ff25b80a49b4e1d
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-01-15 16:21:20 +0100

    script removed

--- build_and_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-./gradlew bootJar --warning-mode all
-cd docker || { echo "Docker folder does not exist."; exit 1; }
-ln ../build/libs/bookworm-backend-0.0.1-SNAPSHOT.jar ./bookworm_backend/bookworm-backend-0.0.1-SNAPSHOT.jar
-cd bookworm_backend
-docker-compose up --build
-unlink bookworm-backend-0.0.1-SNAPSHOT.jar
