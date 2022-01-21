e29b6105bc47b18f5d3c5cca161a1e5a757b32e3
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-01-21 07:14:04 +0100

    security - AuthenticationProvider 4 Keycloak impl

---
+++ src/main/java/org/bookworm/library/security/KeycloakAuthenticationProvider.java
@@ -0,0 +1,17 @@
+package org.bookworm.library.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+
+public class KeycloakAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+        return authentication;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return JwtAuthentication.class.isAssignableFrom(authentication);
+    }
+}
