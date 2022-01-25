5d06c3012b76ebb8e5bf2f727b05943a10bcc54e
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-01-25 08:37:59 +0100

    redis cache config

---
+++ src/main/java/org/bookworm/library/RedisCacheManagerConfiguration.java
@@ -0,0 +1,41 @@
+package org.bookworm.library;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+/**
+ * RedisCacheManagerConfiguration is needed to serialize content of ResponseEntity
+ * returned by methods in controllers (instead of ResponseEntity which is not serializable)
+ * Source: https://stackoverflow.com/questions/41875635/storing-raw-json-in-redis-by-using-spring-data-redis
+ */
+@Configuration
+//@EnableCaching
+public class RedisCacheManagerConfiguration {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public CacheManager redisCacheManager() {
+        RedisSerializationContext.SerializationPair<Object> jsonSerializer =
+                RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(
+                        RedisCacheConfiguration.defaultCacheConfig()
+                                .entryTtl(Duration.ofDays(1))
+                                .serializeValuesWith(jsonSerializer)
+                )
+                .build();
+    }
+
+}
