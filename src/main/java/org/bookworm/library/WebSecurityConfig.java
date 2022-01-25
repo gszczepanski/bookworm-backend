473ff44a8ac0221be699b46c110c669c9f24ca92
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-01-25 08:36:28 +0100

    spring security enabler

---
+++ src/main/java/org/bookworm/library/WebSecurityConfig.java
@@ -0,0 +1,109 @@
+package org.bookworm.library;
+
+import com.auth0.jwk.JwkProvider;
+import lombok.RequiredArgsConstructor;
+import org.bookworm.library.security.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Order(1)
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${keycloak.jwk}")
+    private String jwkProviderUrl;
+
+    /**
+     * No cookies = No CSRF
+     * https://security.stackexchange.com/questions/166724/should-i-use-csrf-protection-on-rest-api-endpoints/166798#166798
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//		http
+//				.csrf().disable()
+//				.authorizeRequests()
+//				.antMatchers("/")
+//				.permitAll();
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable()
+                .cors()
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler())
+                .and()
+                .addFilterBefore(
+                        new AccessTokenFilter(
+                                jwtTokenValidator(keycloakJwkProvider()),
+                                authenticationManagerBean(),
+                                authenticationFailureHandler()),
+                        BasicAuthenticationFilter.class);
+    }
+
+    //swagger-ui excluded from security if development profile enabled
+    @Override
+    @Profile("dev")
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
+
+    @ConditionalOnMissingBean
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return new KeycloakAuthenticationProvider();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new AccessTokenAuthenticationFailureHandler();
+    }
+
+    @Bean
+    public JwtTokenValidator jwtTokenValidator(JwkProvider jwkProvider) {
+        return new JwtTokenValidator(jwkProvider);
+    }
+
+    @Bean
+    public JwkProvider keycloakJwkProvider() {
+        return new KeycloakJwkProvider(jwkProviderUrl);
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new AuthorizationAccessDeniedHandler();
+    }
+}
+
