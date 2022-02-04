73b592cfabb446cea40a4660c3716b7538819328
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-04 16:27:41 +0100

    person repository update

--- src/main/java/org/bookworm/library/repositories/PersonRepository.java
+++ src/main/java/org/bookworm/library/repositories/PersonRepository.java
@@ -2,28 +2,19 @@ package org.bookworm.library.repositories;
 
 import org.bookworm.library.entities.Person;
 import org.bookworm.library.entities.PersonType;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.validation.constraints.NotNull;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, UUID> {
 
-    Page<Person> findAll(@NotNull Pageable pageable);
-
-    Optional<Person> findById(@NotNull UUID id);
-
-    Person save(@NotNull Person person);
-
-    void deleteById(@NotNull UUID id);
-
+    @Transactional
     @Modifying
     @Query("update Person p set p.type = ?1 where p.id = ?2")
     void setType(@NotNull PersonType type, @NotNull UUID id);
