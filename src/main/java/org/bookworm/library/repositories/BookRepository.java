d6d7773c8e2a5b2b77450f210c77f1240ceb1c0f
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-04 16:27:16 +0100

    book repository update

--- src/main/java/org/bookworm/library/repositories/BookRepository.java
+++ src/main/java/org/bookworm/library/repositories/BookRepository.java
@@ -2,15 +2,12 @@ package org.bookworm.library.repositories;
 
 import org.bookworm.library.entities.Book;
 import org.bookworm.library.entities.BookStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.validation.constraints.NotNull;
-import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -18,16 +15,6 @@ import java.util.UUID;
  */
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
-
-    Page<Book> findAll(@NotNull Pageable pageable);
-
-    Optional<Book> findById(@NotNull UUID id);
-
-//    Page<Book> findAllByBorrowerPersonId(Integer borrowerPersonId, Pageable pageable);
-
-    Book save(@NotNull Book book);
-
-    void deleteById(@NotNull UUID id);
 
     @Modifying
     @Query("update Book b set b.status = ?1 where b.id = ?2")
