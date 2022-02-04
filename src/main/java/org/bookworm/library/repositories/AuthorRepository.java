a6e808250e2c21a62fbfab93039f29afb988c10b
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-04 16:26:56 +0100

    author repository update

--- src/main/java/org/bookworm/library/repositories/AuthorRepository.java
+++ src/main/java/org/bookworm/library/repositories/AuthorRepository.java
@@ -1,23 +1,17 @@
 package org.bookworm.library.repositories;
 
 import org.bookworm.library.entities.Author;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.validation.constraints.NotNull;
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
 
-    Page<Author> findAll(@NotNull Pageable pageable);
-
-    Optional<Author> findById(UUID id);
-
-    Author save(@NotNull Author author);
-
-    void deleteById(UUID id);
+    @Query("SELECT a FROM Author a JOIN a.books b WHERE b.id = :bookId")
+    List<Author> findAllByBookId(@NotNull UUID bookId);
 }
