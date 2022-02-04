507d721acd94b217187fd0a87225166b645c60f4
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-04 16:28:18 +0100

    publisher repository update

--- src/main/java/org/bookworm/library/repositories/PublisherRepository.java
+++ src/main/java/org/bookworm/library/repositories/PublisherRepository.java
@@ -1,22 +1,10 @@
 package org.bookworm.library.repositories;
 
 import org.bookworm.library.entities.Publisher;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.validation.constraints.NotNull;
-import java.util.Optional;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Integer> {
 
-    Page<Publisher> findAll(@NotNull Pageable pageable);
-
-    Optional<Publisher> findById(Integer id);
-
-    Publisher save(@NotNull Publisher branch);
-
-    void deleteById(Integer id);
 }
