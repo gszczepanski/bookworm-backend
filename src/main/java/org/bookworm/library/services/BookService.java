327042ffbf9aee3a04cf8a52d5ce66140c7eff3c
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-05 09:44:41 +0100

    book service update

--- src/main/java/org/bookworm/library/services/BookService.java
+++ src/main/java/org/bookworm/library/services/BookService.java
@@ -1,15 +1,20 @@
 package org.bookworm.library.services;
 
 import lombok.RequiredArgsConstructor;
-import org.bookworm.library.dto.BookDto;
+import org.bookworm.library.dto.BookForDisplayDto;
+import org.bookworm.library.dto.BookForModificationDto;
+import org.bookworm.library.dto.BookMapper;
+import org.bookworm.library.dto.BookPatchMapper;
+import org.bookworm.library.entities.Author;
 import org.bookworm.library.entities.Book;
 import org.bookworm.library.entities.BookStatus;
-import org.bookworm.library.mappers.BookMapper;
+import org.bookworm.library.repositories.AuthorRepository;
 import org.bookworm.library.repositories.BookRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -20,14 +25,47 @@ import java.util.UUID;
 public class BookService {
 
     private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
     private final BookMapper bookMapper;
+    private final BookPatchMapper bookPatchMapper;
 
-    public Book save(BookDto bookDto) {
-        return bookRepository.save(bookMapper.toBook(bookDto));
+
+    public BookForModificationDto create(BookForModificationDto bookDto) {
+
+        return bookMapper.toForModificationDto(
+                bookRepository.saveAndFlush(bookMapper.toBook(bookDto))
+        );
     }
 
-    public Page<Book> findAll(Pageable pageable) {
-        return bookRepository.findAll(pageable);
+    public Optional<BookForModificationDto> update(BookForModificationDto bookDto) {
+        Optional<Book> optionalBook = bookRepository.findById(bookDto.getId());
+        if (optionalBook.isPresent()) {
+            return Optional.of(bookMapper.toForModificationDto(
+                    bookRepository.saveAndFlush(bookMapper.toBook(bookDto))
+            ));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<BookForModificationDto> updatePart(BookForModificationDto bookDto) {
+        Optional<Book> optionalBook = bookRepository.findById(bookDto.getId());
+        if (optionalBook.isPresent()) {
+            Book book = bookPatchMapper.updateBookFromDto(bookDto, optionalBook.get());
+            return Optional.of(bookMapper.toForModificationDto(
+                    bookRepository.saveAndFlush(book)
+            ));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Page<BookForDisplayDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).map(bookMapper::toForDisplayDto);
+    }
+
+    public Optional<BookForDisplayDto> findById(UUID id) {
+        return bookRepository.findById(id).map(bookMapper::toForDisplayDto);
     }
 
     public void deleteById(UUID id) {
@@ -36,5 +74,14 @@ public class BookService {
 
     public void setStatus(BookStatus status, UUID id) {
         bookRepository.setStatus(status, id);
+    }
+
+    public void addAuthorToBook(UUID bookId, UUID authorId) {
+        Optional<Author> authorOptional = authorRepository.findById(authorId);
+        Optional<Book> bookOptional = bookRepository.findById(bookId);
+        if (authorOptional.isPresent() && bookOptional.isPresent()) {
+            bookOptional.get().addAuthor(authorOptional.get());
+            bookRepository.save(bookOptional.get());
+        }
     }
 }
