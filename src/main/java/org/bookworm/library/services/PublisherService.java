0d06699a21698cc7f7bcaa5569ef9e0570f8f491
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-05 09:51:25 +0100

    publisher service update

--- src/main/java/org/bookworm/library/services/PublisherService.java
+++ src/main/java/org/bookworm/library/services/PublisherService.java
@@ -2,9 +2,12 @@ package org.bookworm.library.services;
 
 import lombok.RequiredArgsConstructor;
 import org.bookworm.library.dto.PublisherDto;
-import org.bookworm.library.entities.Publisher;
-import org.bookworm.library.mappers.PublisherMapper;
+import org.bookworm.library.dto.PublisherMapper;
 import org.bookworm.library.repositories.PublisherRepository;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -16,25 +19,35 @@ import java.util.Optional;
  */
 @RequiredArgsConstructor
 @Service
+@CacheConfig(cacheNames = {"publishers"})
 public class PublisherService {
 
     private final PublisherRepository publisherRepository;
     private final PublisherMapper publisherMapper;
 
-    public Publisher save(PublisherDto publisherDto) {
-        return publisherRepository.saveAndFlush(publisherMapper.toPublisher(publisherDto));
+
+    @CachePut(key = "#result.id", unless = "#result == null", condition = "#result.id != null")
+    public PublisherDto save(PublisherDto publisherDto) {
+
+        return publisherMapper.toDto(
+                publisherRepository.saveAndFlush(publisherMapper.toPublisher(publisherDto))
+        );
     }
 
-    public Page<Publisher> findAll(Pageable pageable) {
-        return publisherRepository.findAll(pageable);
+    public Page<PublisherDto> findAll(Pageable pageable) {
+
+        return publisherRepository.findAll(pageable).map(publisherMapper::toDto);
     }
 
-    public Optional<Publisher> findById(Integer id) {
-        return publisherRepository.findById(id);
+    @Cacheable(key = "#p0")
+    public Optional<PublisherDto> findById(Integer id) {
+
+        return publisherRepository.findById(id).map(publisherMapper::toDto);
     }
 
+    @CacheEvict(key = "#p0")
     public void deleteById(Integer id) {
-        publisherRepository.deleteById(id);
 
+        publisherRepository.deleteById(id);
     }
 }
