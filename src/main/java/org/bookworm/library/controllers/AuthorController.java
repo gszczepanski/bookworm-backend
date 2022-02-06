3dee8889af9106b50049d564f8f1a9d7d9accb46
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-06 11:56:28 +0100

    author controller update

--- src/main/java/org/bookworm/library/controllers/AuthorController.java
+++ src/main/java/org/bookworm/library/controllers/AuthorController.java
@@ -1,51 +1,73 @@
 package org.bookworm.library.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bookworm.library.dto.AuthorDto;
-import org.bookworm.library.entities.Author;
+import org.bookworm.library.security.roles.AllowedRoles;
 import org.bookworm.library.services.AuthorService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 /**
  * Created by Grzegorz on 2019/06/20
  */
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/authors")
+@CrossOrigin(origins = "${ws.cross.origin.address}")
+@RestController()
+@RequestMapping(value = {"/authors", "/v1/authors"})
+@Slf4j
 public class AuthorController {
 
     private final AuthorService authorService;
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
     @PostMapping(value = "")
-    public Author insert(@RequestBody AuthorDto authorDto) {
-
-        return authorService.save(authorDto);
+    @AllowedRoles("EDITOR")
+    public ResponseEntity<AuthorDto> insert(@RequestBody AuthorDto authorDto) {
+        log.info("Inserting author {}", authorDto);
+        return new ResponseEntity<>(authorService.save(authorDto), HttpStatus.CREATED);
     }
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
-    @PutMapping(value = "/{id}")
-    public Author update(@PathVariable(value = "id") UUID id, @RequestBody AuthorDto authorDto) {
-
-        return authorService.save(authorDto);
+    @PutMapping(value = "")
+    @AllowedRoles("EDITOR")
+    public ResponseEntity<AuthorDto> update(@RequestBody AuthorDto authorDto) {
+        log.info("Updating author {}", authorDto);
+        return new ResponseEntity<>(authorService.save(authorDto), HttpStatus.OK);
     }
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
     @GetMapping(value = "")
-    public Page<Author> findAll(Pageable pageable) {
-
-        return authorService.findAll(pageable);
+    @AllowedRoles("CLIENT")
+    public ResponseEntity<Page<AuthorDto>> findAll(Pageable pageable) {
+        log.info("Find all authors for {}", pageable);
+        Page<AuthorDto> authorDtos = authorService.findAll(pageable);
+        if (!authorDtos.isEmpty()) {
+            return new ResponseEntity<>(authorDtos, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
-    @DeleteMapping(value = "/{id}")
-    public ResponseEntity<String> deleteById(@PathVariable(value = "id") UUID id) {
+    @GetMapping(value = "/{id}")
+    @AllowedRoles("CLIENT")
+    public ResponseEntity<AuthorDto> findById(@PathVariable(value = "id") UUID id) {
+        log.info("Searching for author with id {}", id);
+        Optional<AuthorDto> authorDtoOptional = authorService.findById(id);
+        if (authorDtoOptional.isPresent()) {
+            return new ResponseEntity<>(authorDtoOptional.get(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
+    @DeleteMapping(value = "/{id}")
+    @AllowedRoles("EDITOR")
+    public ResponseEntity<String> deleteById(@PathVariable(value = "id") UUID id) {
+        log.info("Deleting author with id {}", id);
         authorService.deleteById(id);
         return ResponseEntity.ok("author deleted");
     }
