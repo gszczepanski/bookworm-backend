1eab75211999d9a27e98065f3394fdecead7809b
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-06 11:57:49 +0100

    person controller update

--- src/main/java/org/bookworm/library/controllers/PersonController.java
+++ src/main/java/org/bookworm/library/controllers/PersonController.java
@@ -1,63 +1,84 @@
 package org.bookworm.library.controllers;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.bookworm.library.dto.PersonDto;
-import org.bookworm.library.entities.Person;
 import org.bookworm.library.entities.PersonType;
+import org.bookworm.library.security.roles.AllowedRoles;
 import org.bookworm.library.services.PersonService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.transaction.Transactional;
+import java.util.Optional;
 import java.util.UUID;
 
 /**
  * Created by Grzegorz on 2019/06/20
  */
+@SuppressWarnings("checkstyle:Indentation")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "${ws.cross.origin.address}")
 @RestController
-@RequestMapping("/persons")
+@RequestMapping(value = {"/persons", "/v1/persons"})
+@Slf4j
 public class PersonController {
 
     private final PersonService personService;
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
     @PostMapping(value = "")
-    public Person insert(@RequestBody PersonDto personDto) {
-
-        return personService.save(personDto);
+    @AllowedRoles("EDITOR")
+    public ResponseEntity<PersonDto> insert(@RequestBody PersonDto personDto) {
+        log.info("Inserting person {}", personDto);
+        return new ResponseEntity<>(personService.save(personDto), HttpStatus.CREATED);
     }
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
-    @PutMapping(value = "/{id}")
-    public Person update(@PathVariable(value = "id") UUID id, @RequestBody PersonDto personDto) {
-
-        return personService.save(personDto);
+    @PutMapping(value = "")
+    @AllowedRoles("EDITOR")
+    public ResponseEntity<PersonDto> update(@RequestBody PersonDto personDto) {
+        log.info("Updating person {}", personDto);
+        return new ResponseEntity<>(personService.save(personDto), HttpStatus.OK);
     }
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
     @GetMapping(value = "")
-    public Page<Person> findAll(Pageable pageable) {
-
-        return personService.findAll(pageable);
+    @AllowedRoles("CLIENT")
+    public ResponseEntity<Page<PersonDto>> findAll(Pageable pageable) {
+        log.info("Find all persons for {}", pageable);
+        Page<PersonDto> personDtos = personService.findAll(pageable);
+        if (!personDtos.isEmpty()) {
+            return new ResponseEntity<>(personDtos, HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
-    @DeleteMapping(value = "/{id}")
-    @Transactional
-    public ResponseEntity<String> deleteById(@PathVariable(value = "id") UUID id) {
+    @GetMapping(value = "/{id}")
+    @AllowedRoles("CLIENT")
+    public ResponseEntity<PersonDto> findById(@PathVariable(value = "id") UUID id) {
+        log.info("Searching for person with id {}", id);
+        Optional<PersonDto> personDtoOptional = personService.findById(id);
+        if (personDtoOptional.isPresent()) {
+            return new ResponseEntity<>(personDtoOptional.get(), HttpStatus.OK);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
+    @DeleteMapping(value = "/{id}")
+    @AllowedRoles("EDITOR")
+    public ResponseEntity<String> deleteById(@PathVariable(value = "id") UUID id) {
+        log.info("Deleting person with id {}", id);
         personService.deleteById(id);
         return ResponseEntity.ok("person deleted");
     }
 
-    @CrossOrigin(origins = "${ws.cross.origin.address}")
     @PatchMapping(value = "/{id}/type/{type}")
+    @AllowedRoles("EDITOR")
     public ResponseEntity<String> setType(@PathVariable(value = "type") PersonType type,
-                                     @PathVariable(value = "id") UUID id) {
-
+                                          @PathVariable(value = "id") UUID id) {
+        log.info("Setting type {} for person with id {}", type, id);
         personService.setType(type, id);
         return ResponseEntity.ok("person type updated");
     }
