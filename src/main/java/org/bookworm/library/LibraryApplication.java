1012bcf2a83a96165e30c888b45cd9b38566b32f
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-07 19:11:01 +0100

    unnecessary annotation removed, Builder to enable war generation added

--- src/main/java/org/bookworm/library/LibraryApplication.java
+++ src/main/java/org/bookworm/library/LibraryApplication.java
@@ -2,14 +2,22 @@ package org.bookworm.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
-public class LibraryApplication {
+//@EnableCaching // integration tests use @Sql to prepare database before each test and Redis is unable to see it
+public class LibraryApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LibraryApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
     }
 
 }
+
+
