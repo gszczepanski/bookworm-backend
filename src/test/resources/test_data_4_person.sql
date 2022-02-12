37aec11db33c8c59d6aedb98cf742bb4f7474873
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-12 19:11:26 +0100

    person table sample data

---
+++ src/test/resources/test_data_4_person.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.person (id, last_name, middle_name, first_name, id_card_number, id_card_type, type)
+    VALUES ('c0a80015-7d8c-1d2f-817d-8c2e93df2202', 'Reagan', 'Wilson', 'Ronald', '123ABCDE4', 1, 1);
