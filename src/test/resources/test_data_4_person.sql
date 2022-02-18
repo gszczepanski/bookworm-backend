9fe85830e1d26c8d534decc2f64dcdc09b0ae7e6
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-18 07:10:20 +0100

    person controller - test data extended

--- src/test/resources/test_data_4_person.sql
+++ src/test/resources/test_data_4_person.sql
@@ -1,2 +1,4 @@
 INSERT INTO public.person (id, last_name, middle_name, first_name, id_card_number, id_card_type, type)
     VALUES ('c0a80015-7d8c-1d2f-817d-8c2e93df2202', 'Reagan', 'Wilson', 'Ronald', '123ABCDE4', 1, 1);
+INSERT INTO public.person (id, last_name, middle_name, first_name, id_card_number, id_card_type, type)
+    VALUES ('c0a80015-7d8c-1d2f-817d-8c2e93df2212', 'Wilson', null, 'Woodrow', 'A23ABCFE4', 1, 1);
