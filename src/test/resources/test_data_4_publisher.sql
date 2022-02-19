301ec3b7378102a119d50547ca970f0913028ff2
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-19 11:29:31 +0100

    publisher controller - test data extended

--- src/test/resources/test_data_4_publisher.sql
+++ src/test/resources/test_data_4_publisher.sql
@@ -1,2 +1,3 @@
+INSERT INTO public.publisher (id, name) VALUES (5, 'Milano Editores');
 INSERT INTO public.publisher (id, name) VALUES (10, 'Roma Publishing');
 SELECT pg_catalog.setval('public.publisher_id_seq', 11, true);
