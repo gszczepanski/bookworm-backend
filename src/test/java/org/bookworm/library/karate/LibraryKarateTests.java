7b95ec4bd686e5a8e7a4d921d4a0b57f451a66ee
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-21 10:25:33 +0100

    karate tests - executionPhases for sql scripts declared

--- src/test/java/org/bookworm/library/karate/LibraryKarateTests.java
+++ src/test/java/org/bookworm/library/karate/LibraryKarateTests.java
@@ -4,12 +4,18 @@ import com.intuit.karate.junit5.Karate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
-@Sql("classpath:make_tables_empty.sql")
+@SqlGroup({
+        @Sql(scripts = "classpath:make_tables_empty.sql", executionPhase = BEFORE_TEST_METHOD),
+        @Sql(scripts = "classpath:make_tables_empty.sql", executionPhase = AFTER_TEST_METHOD)
+})
 public class LibraryKarateTests {
 
     @Karate.Test
