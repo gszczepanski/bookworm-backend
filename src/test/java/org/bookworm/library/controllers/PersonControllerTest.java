d5c2d6c82555cc49583d6556ecb9e0dfc3dca9c1
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-02-24 08:44:26 +0100

    person controller integration test - refactoring

--- src/test/java/org/bookworm/library/controllers/PersonControllerTest.java
+++ src/test/java/org/bookworm/library/controllers/PersonControllerTest.java
@@ -10,6 +10,7 @@ import org.bookworm.library.utils.IntegrationTest;
 import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +19,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -29,10 +31,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
+@Category(IntegrationTest.class)
 public class PersonControllerTest extends AbstractOAuth2Config {
 
     @Value("${token.test.uri}")
@@ -84,8 +89,10 @@ public class PersonControllerTest extends AbstractOAuth2Config {
     }
 
     @Test
-    @Sql("classpath:make_tables_empty.sql")
-    public void when_save_person_it_should_return_person() {
+    @SqlGroup({
+            @Sql(scripts = "classpath:make_tables_empty.sql", executionPhase = AFTER_TEST_METHOD)
+    })
+    public void save_person_and_return_person() {
         MockMvcResponse response = given()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -103,8 +110,11 @@ public class PersonControllerTest extends AbstractOAuth2Config {
     }
 
     @Test
-    @Sql({"classpath:make_tables_empty.sql", "classpath:test_data_4_person.sql"})
-    public void when_update_one_person_with_put_it_should_return_person() {
+    @SqlGroup({
+            @Sql(scripts = "classpath:test_data_4_person.sql", executionPhase = BEFORE_TEST_METHOD),
+            @Sql(scripts = "classpath:make_tables_empty.sql", executionPhase = AFTER_TEST_METHOD)
+    })
+    public void update_one_person_with_put_and_return_person() {
         MockMvcResponse response = given()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -122,8 +132,11 @@ public class PersonControllerTest extends AbstractOAuth2Config {
     }
 
     @Test
-    @Sql({"classpath:make_tables_empty.sql", "classpath:test_data_4_person.sql"})
-    public void when_find_all_persons_it_should_return_persons_list() {
+    @SqlGroup({
+            @Sql(scripts = "classpath:test_data_4_person.sql", executionPhase = BEFORE_TEST_METHOD),
+            @Sql(scripts = "classpath:make_tables_empty.sql", executionPhase = AFTER_TEST_METHOD)
+    })
+    public void find_all_persons_and_return_persons_list() {
         MockMvcResponse response = given()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -143,8 +156,11 @@ public class PersonControllerTest extends AbstractOAuth2Config {
     }
 
     @Test
-    @Sql({"classpath:make_tables_empty.sql", "classpath:test_data_4_person.sql"})
-    public void when_find_one_person_by_id_it_should_return_person() {
+    @SqlGroup({
+            @Sql(scripts = "classpath:test_data_4_person.sql", executionPhase = BEFORE_TEST_METHOD),
+            @Sql(scripts = "classpath:make_tables_empty.sql", executionPhase = AFTER_TEST_METHOD)
+    })
+    public void find_one_person_by_id_and_return_person() {
         MockMvcResponse response = given()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -163,8 +179,11 @@ public class PersonControllerTest extends AbstractOAuth2Config {
     }
 
     @Test
-    @Sql({"classpath:make_tables_empty.sql", "classpath:test_data_4_person.sql"})
-    public void when_delete_one_person_by_id_it_should_return_ok() {
+    @SqlGroup({
+            @Sql(scripts = "classpath:test_data_4_person.sql", executionPhase = BEFORE_TEST_METHOD),
+            @Sql(scripts = "classpath:make_tables_empty.sql", executionPhase = AFTER_TEST_METHOD)
+    })
+    public void delete_one_person_by_id_and_return_ok() {
         MockMvcResponse response = given()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -180,8 +199,11 @@ public class PersonControllerTest extends AbstractOAuth2Config {
     }
 
     @Test
-    @Sql({"classpath:make_tables_empty.sql", "classpath:test_data_4_person.sql"})
-    public void when_set_one_person_type_it_should_return_ok_and_type() {
+    @SqlGroup({
+            @Sql(scripts = "classpath:test_data_4_person.sql", executionPhase = BEFORE_TEST_METHOD),
+            @Sql(scripts = "classpath:make_tables_empty.sql", executionPhase = AFTER_TEST_METHOD)
+    })
+    public void set_one_person_type_and_return_ok_and_type() {
         MockMvcResponse response = given()
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
