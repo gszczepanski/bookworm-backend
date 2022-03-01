a4821c47035eb3f7ea85391a63b5afa17b57019f
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-03-01 19:43:33 +0100

    run_dev.sh - redis included

--- run_dev.sh
+++ run_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script to run secondary services without bookworm_backend for development purposes
 cd docker || { echo "Docker folder does not exist."; exit 1; }
-docker-compose --env-file .env.dev up bookworm_postgres bookworm_pgadmin bookworm_keycloak
+docker-compose --env-file .env.dev up bookworm_postgres bookworm_pgadmin bookworm_keycloak bookworm_redis
