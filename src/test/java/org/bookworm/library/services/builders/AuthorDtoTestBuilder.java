fb1ff4335e4e26c5526454ee331406fd5c0198ae
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-03-24 09:00:12 +0100

    dto builder for AuthorService unit test

---
+++ src/test/java/org/bookworm/library/services/builders/AuthorDtoTestBuilder.java
@@ -0,0 +1,33 @@
+package org.bookworm.library.services.builders;
+
+import org.bookworm.library.dto.AuthorDto;
+
+import java.util.UUID;
+
+public final class AuthorDtoTestBuilder {
+
+    private static final UUID AUTHOR_UUID = UUID.randomUUID();
+    private static AuthorDto authorDto;
+
+    private static void initializeAutorDto() {
+        String AUTHOR_FIRST_NAME = "Jan";
+        String AUTHOR_LAST_NAME = "Szydło";
+
+        authorDto = new AuthorDto();
+        authorDto.setId(AUTHOR_UUID);
+        authorDto.setDisplayName(AUTHOR_FIRST_NAME + " " + AUTHOR_LAST_NAME);
+        authorDto.setFirstName(AUTHOR_FIRST_NAME);
+        authorDto.setLastName(AUTHOR_LAST_NAME);
+    }
+
+    public static AuthorDto standardItem() {
+        if (authorDto == null) {
+            initializeAutorDto();
+        }
+        return authorDto;
+    }
+
+    public static UUID standardKey() {
+        return AUTHOR_UUID;
+    }
+}
