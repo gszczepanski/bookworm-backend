b60b9da2ae072c12f0c011d57b4fb5c9e80cca55
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-04-22 08:31:29 +0200

    @SuperBuilder included for testability

--- src/main/java/org/bookworm/library/dto/BookForModificationDto.java
+++ src/main/java/org/bookworm/library/dto/BookForModificationDto.java
@@ -1,6 +1,7 @@
 package org.bookworm.library.dto;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.io.Serializable;
 
@@ -8,6 +9,7 @@ import java.io.Serializable;
  * Created by Grzegorz on 2020/05/25
  */
 @NoArgsConstructor
+@SuperBuilder
 @Getter
 @Setter
 @ToString(callSuper = true)
