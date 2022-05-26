020016ccd9a886576a8db99f39c84ddeb2132b6c
grzegorz <mailbox.g.szczepanski@gmail.com>
2022-05-26 07:53:55 +0200

    person test object builder - properties

--- src/test/java/org/bookworm/library/services/builders/PersonDtoEasyTestBuilder.java
+++ src/test/java/org/bookworm/library/services/builders/PersonDtoEasyTestBuilder.java
@@ -1,4 +1,16 @@
 package org.bookworm.library.services.builders;
 
+import com.natpryce.makeiteasy.Property;
+import org.bookworm.library.dto.PublisherDto;
+
+import static com.natpryce.makeiteasy.Property.newProperty;
+
 public class PersonDtoEasyTestBuilder {
+    public static final Property<PublisherDto, Integer> PersonID = newProperty();
+    public static final Property<PublisherDto, String> LastName = newProperty();
+    public static final Property<PublisherDto, String> MiddleName = newProperty();
+    public static final Property<PublisherDto, String> FirstName = newProperty();
+    public static final Property<PublisherDto, String> IdCardNumber = newProperty();
+    public static final Property<PublisherDto, String> IdCardType = newProperty();
+    public static final Property<PublisherDto, String> Type = newProperty();
 }
